519bce3494baa99e4e02bf0d50cc4a29ea988535
alejandromartincruz <socorreitor@gmail.com>
2015-09-24 19:54:21 +0200

    Added second and third iteration sollutions

--- caesar.rb
+++ caesar.rb
@@ -1,42 +1,39 @@
-def decode(word)
+def decode(word, number)
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]	
 j = word.length
-puts "The word has a length of #{j}"
 
 for	i in 0..j-1
+	if word[i] != " "
+		
 	k = 0
 	while word[i] != alphabet[k]
 		k = k + 1
 	end
 	if word[i] == alphabet[k]
-		word[i] = alphabet[k-1]
+		word[i] = alphabet[k + number]
+	end
 	end
 end
 word = word.join
 puts word
 end
 
-puts "write a word to decode"
+puts "write a sentence or a word to decode:"
 
 word = gets.chomp
 word = word.downcase
 word = word.split("")
-puts "You want to decode #{word}"
-decode(word)
+#puts "You want to decode #{word}"
+
+puts "Enter the number of shifts required to decode the message"
+puts "Negative number for left shits, and positive numbers for right shifts"
+#force number to -3 with number=-3 for the third iteration of the exercise that requires always a 3 left shift
+number = gets.chomp.to_i
+#number = number * (-1)
+
+decode(word, number)
 
 
-
-
-=begin
-def wordSorting(sentence)
-	
-sentence.gsub!(/\p{^Alnum}/, ' ')
-array_sentence = sentence.split(" ")
-puts array_sentence.sort! { |a, b| a.downcase <=> b.downcase }
-end
-
-puts "Write a sentence"
-sentence = gets.chomp
-wordSorting sentence
-=end
+#With the next line can be deleted all the non alphanumerical characters of the sentence
+#word.gsub!(/\p{^Alnum}/, ' ')
